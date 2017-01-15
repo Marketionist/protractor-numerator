edd4ee771b3d165fc7864ac77a6d5f4af1a62e42
Marketionist <marketionist@gmail.com>
2017-01-15 14:08:57 +0200

    Refactored tests to use selectorLinkCSS and selectorLinkXPath

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,7 @@
 describe('protractor-numerator tests', function () {
+    let selectorLinkCSS = '.site-footer-links > li';
+    let selectorLinkXPath = '//li[ancestor::*[contains(@class, "site-footer-links")]]';
+
     beforeAll(function () {
         // Turn off Angular synchronization
         browser.ignoreSynchronization = true;
@@ -12,20 +15,19 @@ describe('protractor-numerator tests', function () {
     });
 
     it('should get the first link from the Github main page footer', function () {
-        let linkFooterContactGitHub = element.all(by.css('.site-footer-links > li')).first();
+        let linkFooterContactGitHub = element.all(by.css(selectorLinkCSS)).first();
 
         expect(linkFooterContactGitHub.getText()).toBe('Contact GitHub');
     });
 
     it('should get the second link from the Github main page footer', function () {
-        let linkFooterAPI = element.all(by.xpath(
-            '//li[ancestor::*[contains(@class, "site-footer-links")]]')).second();
+        let linkFooterAPI = element.all(by.xpath(selectorLinkXPath)).second();
 
         expect(linkFooterAPI.getText()).toBe('API');
     });
 
     it('should get the third link from the Github main page footer', function () {
-        let linkFooterTraining = element.all(by.css('.site-footer-links > li')).third();
+        let linkFooterTraining = element.all(by.css(selectorLinkCSS)).third();
 
         expect(linkFooterTraining.getText()).toBe('Training');
     });
